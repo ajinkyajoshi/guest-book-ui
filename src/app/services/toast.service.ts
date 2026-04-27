8ed6842f3da9584b756153c0732638dbbd51140d
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2026-04-27 16:56:11 -0400

    Create toast.service.ts

---
+++ src/app/services/toast.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface Toast {
+  message: string;
+  type: 'success' | 'error' | 'info';
+}
+
+@Injectable({ providedIn: 'root' })
+export class ToastService {
+  private toastSubject = new Subject<Toast>();
+  toast$ = this.toastSubject.asObservable();
+
+  show(message: string, type: Toast['type'] = 'success') {
+    this.toastSubject.next({ message, type });
+  }
+}
